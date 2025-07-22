a3205c2b055a290f3411de0a0579bf8484bbbd76
Gustavo Petrolini <guspetrolini@hotmail.com>
2025-07-22 16:57:26 -0300

    Feat: Aula 20 Barra Lateral(Menu)  - Cria o menu de navegação lateral
    Implementa a estrutura principal de navegação da aplicação, que
    ficará visível em todas as páginas internas do sistema.
    
    - Cria o arquivo `left.php` contendo a estrutura HTML do menu.
    - Utiliza as tags semânticas `<aside>` e `<nav>` para a estrutura do menu.
    - Adiciona os links de navegação para 'Registrar Ponto', 'Relatório Mensal', 'Relatório Gerencial' e 'Usuários'.
    - Atualiza a função `load_template_view()` para incluir o menu lateral no layout padrão, entre o header e o conteúdo principal.

---
+++ In-N-Out/src/views/template/left.php
@@ -0,0 +1,30 @@
+<aside class="sidebar">
+    <nav class="menu mt-3">
+        <ul class="nav-list">
+            <li class="nav-list">
+                <a href="day_records.php" class="link">
+                    <i class="icofont-ui-check mr-2"></i>
+                    Registrar Ponto
+                </a>
+            </li>
+            <li class="nav-list">
+                <a href="day_records.php" class="link">
+                    <i class="icofont-ui-calendar mr-2"></i>
+                    Relatório Mensal
+                </a>
+            </li>
+            <li class="nav-list">
+                <a href=".php" class="link">
+                    <i class="icofont-chart-histogram mr-2"></i>
+                    Relatório Gerencial
+                </a>
+            </li>
+            <li class="nav-list">
+                <a href=".php" class="link">
+                    <i class="icofont-users mr-2"></i>
+                    Usuários
+                </a>
+            </li>
+        </ul>
+    </nav>
+</aside>
