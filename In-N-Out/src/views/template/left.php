6e14ce8cd160abb6b65ba284925d46afc91546ca
Gustavo Petrolini <guspetrolini@hotmail.com>
2025-07-24 14:30:41 -0300

    Feat: Aula 41 Exibir horas calculadas - Exibe os cálculos de horas trabalhadas e saída na sidebar
    Integra a lógica de cálculo de horas do back-end com a interface do
    usuário. Os widgets na barra lateral agora exibem dinamicamente o
    total de horas trabalhadas e a hora de saída projetada para o dia.
    
    - Centraliza a lógica de carregamento do registro de ponto diário (`WorkingHours`) no arquivo `loader.php`.
    - Garante que os dados de horas e saída estejam disponíveis em todas as páginas que usam o layout padrão.
    - A função `load_template_view` agora calcula e prepara as variáveis `$workedInterval` e `$exitTime`.
    - Atualiza os widgets na barra lateral (`left.php`) para exibir dinamicamente os valores dessas novas variáveis.
    - Remove a lógica duplicada de carregamento de dados do `DailyRecordsController`.

--- In-N-Out/src/views/template/left.php
+++ In-N-Out/src/views/template/left.php
@@ -32,7 +32,7 @@
             <i class="icon icofont-hour-glass text-primary"></i>
             <div class="info">
                 <span class="main text-primary">
-                    04:23
+                    <?php $workedInterval ?>
                 </span>
                 <span class="label text-muted">Horas Trabalhadas</span>
             </div>
@@ -43,7 +43,7 @@
                 <i class="icon icofont-ui-alarm text-danger"></i>
                 <div class="info">
                 <span class="main text-danger">
-                    04:23
+                    <?php $exitTime ?>
                 </span>
                     <span class="label text-muted">Hora de Saída</span>
                 </div>
