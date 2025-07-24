6851763ce52c0e445d746427beb6912f7d4db9d5
Gustavo Petrolini <guspetrolini@hotmail.com>
2025-07-24 16:09:16 -0300

    Fix(reports): Aula 47 Relatório Mensal #03 - Processa dados e calcula saldo de horas do relatório mensal
    Implementa a lógica de negócio no `MonthlyReportController` para
    processar os registros de ponto e calcular os totais para a
    exibição no relatório, como a soma das horas trabalhadas e o
    saldo de horas do mês.
    
    - O controller agora itera por todos os dias do mês para consolidar os dados.
    - Para dias úteis no passado sem registro de ponto, um objeto `WorkingHours` vazio é criado para representar a ausência.
    - Calcula o total de horas trabalhadas no mês (`sumOfWorkedTime`).
    - Calcula o total de horas esperadas (`expectedTime`) com base nos dias úteis que já passaram.
    - Calcula o saldo final de horas (`balance`), que pode ser positivo ou negativo.
    - Adiciona as funções `is_past_workday` e `get_time_string_from_seconds` em `date_utils.php` para auxiliar nos cálculos e formatação.
    - O controller agora passa todos os dados processados e consolidados para a view.

--- In-N-Out/src/controllers/monthly_report.php
+++ In-N-Out/src/controllers/monthly_report.php
@@ -2,10 +2,40 @@
 session_start();
 requireValidSession();
 
-$user = $_SESSION['user'];
+$currentDate = new DateTime();
 
+$user = $_SESSION['user'];
 $registries = WorkingHours::getMonthlyReport($user->Id, new DateTime());
 
+$report = [];
+$workday = 0;
+$sumOfWorkedDay = 0;
+$lastDay = getLastDayOfMonth($currentDate)->format('d');
+
+for($day = 1; $day <= $lastDay; $day++)
+{
+    $date = $currentDate->format("Y-m") . '-' . sprintf("%02d", $day);
+    $registry = $registries[$date];
+
+    if(isPastWorkday($date)) $workday++;
+
+    if($registry) {
+        $sumOfWorkedDay += $registry->worked_time;
+        array_push($report, $registry);
+    } else {
+        array_push($report, new WorkingHours([
+            'work_date' => $date,
+            'worked_time' => 0
+        ]));
+    }
+}
+
+$expectedTime = $workday * DAILY_TIME;
+$balance = getTimeStringFromSeconds(abs($sumOfWorkedTime - $expectedTime));
+$sign = ($sumOfWorkedTime > $expectedTime) ? "+" : '-';
+
 loadTemplateView('monthly_report', [
-    'registries' => $registries,
+    'report' => $report,
+    'sumOfWorkedTime' => $sumOfWorkedTime,
+    'balance' => "{$sign}{$balance}"
 ]);
