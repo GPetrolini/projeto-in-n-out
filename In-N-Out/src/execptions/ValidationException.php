17cc3b6933d57a04550ed26ce39ff221412c033b
Gustavo Petrolini <guspetrolini@hotmail.com>
2025-07-25 16:10:16 -0300

    Refactor (exception)

--- In-N-Out/src/execptions/ValidationException.php
+++ In-N-Out/src/execptions/ValidationException.php
@@ -1,26 +1,22 @@
 <?php
 
+declare(strict_types=1);
+
 class ValidationException extends AppException
 {
 
     private $errors = [];
-    public function __construct
-    ($errors = [],
-     $message = ' Erros de validação',
-     $code = 0,
-     $previous = null
-    )
-    {
+
+    public function __construct($errors = [], $message = 'Erros de validação', $code = 0, $previous = null) {
         parent::__construct($message, $code, $previous);
         $this->errors = $errors;
     }
 
-    public function getErrors()
-    {
+    public function getErrors() {
         return $this->errors;
     }
-    public function get($att)
-    {
+
+    public function get($att) {
         return $this->errors[$att];
     }
 }
