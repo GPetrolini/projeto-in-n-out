74f46f401b857b593b69ba4d4fbdcd53b3542498
Gustavo Petrolini <guspetrolini@hotmail.com>
2025-07-25 16:18:50 -0300

    Refactor (views)

--- In-N-Out/src/views/day_records.php
+++ In-N-Out/src/views/day_records.php
@@ -1,43 +1,43 @@
 <main class="content">
     <?php
-        renderTitle('Registrar ponto',
-            'Mantenha seu ponto consistente',
-            'icofont-check-alt'
-        );
-        include(TEMPLATE_PATH . "/messages.php");
+    renderTitle(
+        'Registrar Ponto',
+        'Mantenha seu ponto consistente!',
+        'icofont-check-alt'
+    );
+    include(TEMPLATE_PATH . "/messages.php");
     ?>
     <div class="card">
         <div class="card-header">
-            <h3><?php $today ?></h3>
-            <p class="mb-0">Os batimentos efetuados</p>
+            <h3><?= $today ?></h3>
+            <p class="mb-0">Os batimentos efetuados hoje</p>
         </div>
         <div class="card-body">
             <div class="d-flex m-5 justify-content-around">
-            <span>Entrada 1: <?php $records->time1 ?? '---' ?></span>
-            <span>Saída 1: <?php $records->time2 ?? '---' ?></span>
+                <span class="record">Entrada 1: <?= $workingHours->time1 ?? '---' ?></span>
+                <span class="record">Saída 1: <?= $workingHours->time2 ?? '---' ?></span>
             </div>
-        </div>
-        <div class="card-body">
             <div class="d-flex m-5 justify-content-around">
-            <span>Entrada 2: <?php $records->time3 ?? '---' ?></span>
-            <span>Saída 2: <?php $records->time4 ?? '---' ?></span>
+                <span class="record">Entrada 2: <?= $workingHours->time3 ?? '---' ?></span>
+                <span class="record">Saída 2: <?= $workingHours->time4 ?? '---' ?></span>
             </div>
         </div>
-        <div class="card-footer d-flex">
+        <div class="card-footer d-flex justify-content-center">
             <a href="innout.php" class="btn btn-success btn-lg">
                 <i class="icofont-check mr-1"></i>
-                Bater ponto
+                Bater o Ponto
             </a>
         </div>
     </div>
 
     <form class="mt-5" action="innout.php" method="post">
         <div class="input-group no-border">
-            <input type="text" name="forcedTime" class="form-control" placeholder="Informe a hora para simular o batimento">
-            <button class="btn btn-danger ml-3" >
+            <input type="text" name="forcedTime" class="form-control"
+                   placeholder="Informe a hora para simular o batimento">
+            <button class="btn btn-danger ml-3">
                 Simular Ponto
             </button>
         </div>
     </form>
-    
+
 </main>
--- In-N-Out/src/views/manager_report.php
+++ In-N-Out/src/views/manager_report.php
@@ -1,50 +1,50 @@
 <main class="content">
     <?php
-        renderTitle(
-            'Relatório Gerencial',
-            'Resumo das horas trabalhadas dos funcionários',
-            'icofont-chart-histogram'
-        );
+    renderTitle(
+        'Relatório Gerencial',
+        'Resumo das horas trabalhadas dos funcionários',
+        'icofont-chart-histogram'
+    );
     ?>
-
 
     <div class="summary-boxes">
         <div class="summary-box bg-primary">
-            <i class="icofont-users"></i>
-            <p class="title">Qtde de funcionários</p>
-            <h3 class="value"><?php $activeUsersCount ?></h3>
+            <i class="icon icofont-users"></i>
+            <p class="title">Qtde de Funcionários</p>
+            <h3 class="value"><?= $activeUsersCount ?></h3>
         </div>
         <div class="summary-box bg-danger">
-            <i class="icofont-patient-bed"></i>
+            <i class="icon icofont-patient-bed"></i>
             <p class="title">Faltas</p>
-            <h3 class="value"><?php $absentUsers ?></h3>
+            <h3 class="value"><?= count($absentUsers) ?></h3>
         </div>
         <div class="summary-box bg-success">
-            <i class="icofont-sand-clock"></i>
-            <p class="title">Horas Trabalhadas no Mes</p>
-            <h3 class="value"><?php $hoursInMonth ?></h3>
+            <i class="icon icofont-sand-clock"></i>
+            <p class="title">Horas no Mês</p>
+            <h3 class="value"><?= $hoursInMonth ?></h3>
         </div>
     </div>
-    <?php  if(count($absentUsers) > 0) : ?>
+
+    <?php if(count($absentUsers) > 0): ?>
         <div class="card mt-4">
             <div class="card-header">
-                <h4 class="card-title"> Faltosos do Dia</h4>
-                <p class="card-category">Relação dos funcionários que não bateram ponto</p>
+                <h4 class="card-title">Faltosos do Dia</h4>
+                <p class="card-category mb-0">Relação dos funcionários que ainda não bateram o ponto</p>
             </div>
             <div class="card-body">
                 <table class="table table-bordered table-striped table-hover">
                     <thead>
-                        <th>Nome</th>
+                    <th>Nome</th>
                     </thead>
                     <tbody>
-                        <?php foreach($absentUsers as $name): ?>
-                            <tr>
-                                <td><?php $name ?></td>
-                            </tr>
-                        <?php endforeach?>
+                    <?php foreach($absentUsers as $name): ?>
+                        <tr>
+                            <td><?= $name ?></td>
+                        </tr>
+                    <?php endforeach ?>
                     </tbody>
                 </table>
             </div>
         </div>
-    <?php endif?>
+    <?php endif ?>
 </main>
