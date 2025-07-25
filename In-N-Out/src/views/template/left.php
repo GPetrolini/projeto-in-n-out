dde25e1ea01c1729fdbc988f02990552f4e359d1
Gustavo Petrolini <guspetrolini@hotmail.com>
2025-07-25 16:17:05 -0300

    Refactor (views/template)

--- In-N-Out/src/views/template/left.php
+++ In-N-Out/src/views/template/left.php
@@ -1,31 +1,31 @@
 <aside class="sidebar">
     <nav class="menu mt-3">
         <ul class="nav-list">
-            <li class="nav-list">
-                <a href="day_records.php" class="link">
+            <li class="nav-item">
+                <a href="day_records.php">
                     <i class="icofont-ui-check mr-2"></i>
                     Registrar Ponto
                 </a>
             </li>
-            <li class="nav-list">
-                <a href="../monthly_report.php" class="link">
+            <li class="nav-item">
+                <a href="monthly_report.php">
                     <i class="icofont-ui-calendar mr-2"></i>
                     Relatório Mensal
                 </a>
             </li>
-            <li class="nav-list">
-                <a href="manager_report.php" class="link">
-                    <i class="icofont-chart-histogram mr-2"></i>
-                    Relatório Gerencial
-                </a>
-            </li>
             <?php if($user->is_admin): ?>
-            <li class="nav-list">
-                <a href="users.php" class="link">
-                    <i class="icofont-users mr-2"></i>
-                    Usuários
-                </a>
-            </li>
+                <li class="nav-item">
+                    <a href="manager_report.php">
+                        <i class="icofont-chart-histogram mr-2"></i>
+                        Relatório Gerencial
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="users.php">
+                        <i class="icofont-users mr-2"></i>
+                        Usuários
+                    </a>
+                </li>
             <?php endif ?>
         </ul>
     </nav>
@@ -34,23 +34,22 @@
             <i class="icon icofont-hour-glass text-primary"></i>
             <div class="info">
                 <span class="main text-primary"
-                    <?php $activeClock === 'workedInterval' ? 'active-clock' : '' ?>>
-                    <?php $workedInterval ?>
+                    <?= $activeClock === 'workedInterval' ? 'active-clock' : '' ?>>
+                    <?= $workedInterval ?>
                 </span>
                 <span class="label text-muted">Horas Trabalhadas</span>
             </div>
         </div>
         <div class="division my-3"></div>
-        <div class="sidebar-widgets">
-            <div class="sidebar-widget">
-                <i class="icon icofont-ui-alarm text-danger"></i>
-                <div class="info">
+        <div class="sidebar-widget">
+            <i class="icon icofont-ui-alarm text-danger"></i>
+            <div class="info">
                 <span class="main text-danger"
-                    <?php $activeClock === 'exitTime' ? 'active-clock' : '' ?>>
-                    <?php $exitTime ?>
+                    <?= $activeClock === 'exitTime' ? 'active-clock' : '' ?>>
+                    <?= $exitTime ?>
                 </span>
-                    <span class="label text-muted">Hora de Saída</span>
-                </div>
+                <span class="label text-muted">Hora de Saída</span>
             </div>
+        </div>
     </div>
 </aside>
--- In-N-Out/src/views/template/messages.php
+++ In-N-Out/src/views/template/messages.php
@@ -1,24 +1,24 @@
 <?php
 $errors = [];
 
-if (isset($_SESSION['message'])) {
+
+if(isset($_SESSION['message'])) {
     $message = $_SESSION['message'];
     unset($_SESSION['message']);
 } elseif($exception) {
     $message = [
-        'type' => 'danger',
+        'type' => 'error',
         'message' => $exception->getMessage()
     ];
-    if(get_class($exception) === 'ValidationException')
-    {
+
+    if(get_class($exception) === 'ValidationException') {
         $errors = $exception->getErrors();
     }
 }
 
 $alertType = '';
 
-if($message['type'] === 'error')
-{
+if($message['type'] === 'error') {
     $alertType = 'danger';
 } else {
     $alertType = 'success';
@@ -26,8 +26,8 @@ if($message['type'] === 'error')
 ?>
 
 <?php if($message): ?>
-<div role="alert" class="my-3 alert alert-
-        <?php $alertType ?>" >
-        <?php $message['message'] ?>
-</div>
-<?php endif; ?>
+    <div role="alert"
+         class="my-3 alert alert-<?= $alertType ?>">
+        <?= $message['message'] ?>
+    </div>
+<?php endif ?>
